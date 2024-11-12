2da694a7fe0dcffc8817cbdd73fd9935a59a89b1
benbarnett02 <benjamin.barnett02@gmail.com>
2024-11-12 19:45:20 +1100

    updated client class to remove TS warnings when using constructor - assertion that the private _email and _phoneNumber backing fields are always set

--- PROG2005-A1-P1/Client.ts
+++ PROG2005-A1-P1/Client.ts
@@ -1,34 +1,44 @@
-export  class Client{
-   public id: string; // Using a string - we're never going to do math on it so why constrain ourselves to a number?
+export class Client {
+    public readonly id: string; // Using a string - we're never going to do math on it so why constrain ourselves to a number?
     public name: string;
     public gender: enumGender;
     public dateOfBirth: Date;
     public program: enumProgram;
 
-    private _email: string;
-    private _phoneNumber: string;
+    private _email!: string;
+    private _phoneNumber!: string;
 
     public startDate: Date;
     public endDate: Date;
 
     public notes?: string;
+    public vip: boolean = false;
 
+    public get email(): string {
+        return this._email;
+    }
 
-    public get email(): string { return this._email; }
     public set email(value: string) {
         if (value.length > 0 && value.indexOf("@") > 0) {
             this._email = value;
+        } else {
+            throw new Error("Invalid email address");
         }
     }
 
-    public get phoneNumber(): string { return this._phoneNumber; }
+    public get phoneNumber(): string {
+        return this._phoneNumber;
+    }
+
     public set phoneNumber(value: string) {
         if (value.length == 10 && value.indexOf("04") == 0) { // Maybe we only want Australian numbers & no country code (start with 04)
             this._phoneNumber = value;
+        } else {
+            throw new Error("Invalid phone number");
         }
     }
 
-    constructor(id: string, name: string, gender: enumGender, dateOfBirth: Date, program: enumProgram, email: string, phoneNumber: string, startDate: Date, endDate: Date, notes?: string) {
+constructor(id: string, name: string, gender: enumGender, dateOfBirth: Date, program: enumProgram, email: string, phoneNumber: string, startDate: Date, endDate: Date, notes?: string) {
         this.id = id;
         this.name = name;
         this.gender = gender;
@@ -43,13 +53,13 @@ export  class Client{
 
 }
 
-enum enumGender {
+export enum enumGender {
     unspecified = "Unspecified",
     female = "Female",
     male = "Male"
 }
 
-enum enumProgram {
+export enum enumProgram {
     unspecified = "Unspecified",
     FatLoss = "Fat Loss",
     Senior = "Senior Fitness",
