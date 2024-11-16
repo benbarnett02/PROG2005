67dadd3fabe5af1c34491dd09d113f106127ea7f
benbarnett02 <benjamin.barnett02@gmail.com>
2024-11-17 10:18:10 +1100

    Made an about page. Added horizontla scroll on vip client page

--- PROG2005-A1-P2/src/app/pages/vip-client/vip-client.component.ts
+++ PROG2005-A1-P2/src/app/pages/vip-client/vip-client.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {ClientListComponent} from '../../shared/client-list/client-list.component';
 import {Client, ClientService} from '../../services/client.service';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-vip-client',
   standalone: true,
   imports: [
-    ClientListComponent
+    ClientListComponent,
+    RouterLink
   ],
   templateUrl: './vip-client.component.html',
   styleUrl: './vip-client.component.css'
